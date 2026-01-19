3d3c3b27cac1e0a7c3805183f554ae43554814a3
mashed26 <100186141+mashed26@users.noreply.github.com>
2026-01-18 20:18:59 -0600

    added hood

--- src/main/java/frc2026/robot/subsystems/shooter/hood/HoodConstants.java
+++ src/main/java/frc2026/robot/subsystems/shooter/hood/HoodConstants.java
@@ -1,3 +1,30 @@
 package frc2026.robot.subsystems.shooter.hood;
 
-public class HoodConstants {}
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.teamscreamrobotics.drivers.TalonFXSubsystem.TalonFXSubsystemConfiguration;
+import com.teamscreamrobotics.drivers.TalonFXSubsystem.CANCoderConstants;
+import com.teamscreamrobotics.drivers.TalonFXSubsystem.CANDevice;
+import com.teamscreamrobotics.drivers.TalonFXSubsystem.TalonFXConstants;
+
+public class HoodConstants {
+    // TODO: Put in actual values
+    public static final double HOOD_REDUCTION = 0;
+
+    public static final TalonFXSubsystemConfiguration HOOD_CONFIG = new TalonFXSubsystemConfiguration();
+
+    static {
+        HOOD_CONFIG.name = "Hood";
+
+        HOOD_CONFIG.codeEnabled = true;
+        HOOD_CONFIG.logTelemetry = false;
+        HOOD_CONFIG.debugMode = false;
+
+        HOOD_CONFIG.masterConstants = new TalonFXConstants(new CANDevice(10), InvertedValue.CounterClockwise_Positive);
+        
+        HOOD_CONFIG.neutralMode = NeutralModeValue.Brake;
+        HOOD_CONFIG.rotorToSensorRatio = HOOD_REDUCTION;
+        HOOD_CONFIG.enableSupplyCurrentLimit = true;
+        HOOD_CONFIG.supplyCurrentLimit = 35;
+    }
+}
