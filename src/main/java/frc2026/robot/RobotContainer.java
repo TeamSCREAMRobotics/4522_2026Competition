e4604e3113a509267fe6f86e36c1455a83d7646b
mashed26 <100186141+mashed26@users.noreply.github.com>
2026-01-22 21:52:45 -0600

    Added Vision Manager and Drivetrain

--- src/main/java/frc2026/robot/RobotContainer.java
+++ src/main/java/frc2026/robot/RobotContainer.java
@@ -6,8 +6,13 @@ package frc2026.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc2026.robot.subsystems.drivetrain.Drivetrain;
+import frc2026.robot.subsystems.drivetrain.generated.TunerConstants;
 
 public class RobotContainer {
+
+  Drivetrain drivetrain = TunerConstants.drivetrain;
+
   public RobotContainer() {
     configureBindings();
   }
--- src/main/java/frc2026/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc2026/robot/subsystems/vision/VisionConstants.java
@@ -1,3 +1,11 @@
 package frc2026.robot.subsystems.vision;
 
-public class VisionConstants {}
+public class VisionConstants {
+  public static final int resolutionWidth = 1280;
+  public static final int resolutionHeight = 960;
+
+  public static final double xyStdBaseline = 0.93;
+  public static final double thetaStdBaseline = 12.5;
+
+  public static final double xyMt2StdFactor = 0.3;
+}
