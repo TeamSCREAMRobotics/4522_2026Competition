acdebf4b1842ffdab311eaccfac1d922354f5a8f
TeamSCREAMBot <teamscreamprogramming@gmail.com>
2026-02-19 05:50:59 +0000

    Apply Spotless formatting

--- src/main/java/frc2026/tars/Robot.java
+++ src/main/java/frc2026/tars/Robot.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc2026.tars.controlboard.Dashboard;
-import frc2026.tars.subsystems.vision.VisionManager;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
