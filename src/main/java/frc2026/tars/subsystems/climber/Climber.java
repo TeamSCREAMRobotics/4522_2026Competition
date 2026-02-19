4a58e1098a3feb24ceef6281cd7dbe58396414c7
mashed26 <100186141+mashed26@users.noreply.github.com>
2026-02-19 00:38:08 -0600

    added zeroing logic

--- src/main/java/frc2026/tars/subsystems/climber/Climber.java
+++ src/main/java/frc2026/tars/subsystems/climber/Climber.java
@@ -3,6 +3,11 @@ package frc2026.tars.subsystems.climber;
 import com.teamscreamrobotics.data.Length;
 import com.teamscreamrobotics.drivers.TalonFXSubsystem;
 import com.teamscreamrobotics.math.Conversions;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import java.util.function.DoubleSupplier;
 
 public class Climber extends TalonFXSubsystem {
@@ -39,5 +44,20 @@ public class Climber extends TalonFXSubsystem {
     public DoubleSupplier feedForward() {
       return () -> 0;
     }
+  }
+
+  private double startTime = 0.0;
+
+  public Command zero() {
+
+    return new SequentialCommandGroup(
+        new InstantCommand(() -> startTime = Timer.getFPGATimestamp()),
+        applyVoltageCommand(() -> -1.0)
+            .withDeadline(
+                new WaitUntilCommand(
+                    () ->
+                        ((Timer.getFPGATimestamp() - startTime) > 0.5)
+                            && master.getSupplyCurrent().getValueAsDouble() > 1.8)),
+        new InstantCommand(() -> resetPosition(0)));
   }
 }
