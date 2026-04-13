6be4781a8dc04e7a3a454cb90e7a2b7d49edcc5d
TeamSCREAMBot <teamscreamprogramming@gmail.com>
2026-04-13 15:36:26 +0000

    Apply Spotless formatting

--- src/main/java/frc2026/tars/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc2026/tars/subsystems/drivetrain/Drivetrain.java
@@ -45,8 +45,10 @@ import lombok.Getter;
  */
 public class Drivetrain extends TunerSwerveDrivetrain implements Subsystem {
   public static final double kSimLoopPeriod = 0.004; // 4 ms
+
   @SuppressWarnings("unused")
   private Notifier m_simNotifier = null;
+
   private double m_lastSimTime;
 
   @Getter private final PhoenixSwerveHelper helper;
