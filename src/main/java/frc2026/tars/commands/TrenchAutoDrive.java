5ccd633ea26c9d45701fb522dec798cabf2b9b10
mashed26 <100186141+mashed26@users.noreply.github.com>
2026-04-21 15:32:35 -0500

    pathing

---
+++ src/main/java/frc2026/tars/commands/TrenchAutoDrive.java
@@ -0,0 +1,55 @@
+package frc2026.tars.commands;
+
+import java.util.function.Supplier;
+
+import com.teamscreamrobotics.gameutil.FieldConstants;
+import com.teamscreamrobotics.util.AllianceFlipUtil;
+import com.teamscreamrobotics.util.BLinePathSequence;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import frc.robot.lib.BLine.FollowPath;
+import frc.robot.lib.BLine.FlippingUtil.FieldSymmetry;
+import frc2026.tars.subsystems.drivetrain.Drivetrain;
+
+public class TrenchAutoDrive {
+    private final Drivetrain drivetrain;
+    private final FollowPath.Builder pathBuilder;
+
+    private final BLinePathSequence rightAllianceToNeutral;
+    private final BLinePathSequence leftAllianceToNeutral;
+    private final BLinePathSequence rightNeutralToAlliance;
+    private final BLinePathSequence leftNeutralToAlliance;
+
+    public TrenchAutoDrive(Drivetrain drivetrain) {
+        this.drivetrain = drivetrain;
+
+        pathBuilder =
+        new FollowPath.Builder(
+                drivetrain,
+                drivetrain::getEstimatedPose,
+                () -> drivetrain.getState().Speeds,
+                (speeds) ->
+                    drivetrain.setControl(drivetrain.getHelper().getApplyRobotSpeeds(speeds)),
+                new PIDController(5.0, 0.0, 0.0),
+                new PIDController(3.0, 0.0, 0.0),
+                new PIDController(2.0, 0.0, 0.0))
+            .withDefaultShouldFlip();
+
+        rightAllianceToNeutral = new BLinePathSequence(pathBuilder, FieldSymmetry.kRotational, "");
+        leftAllianceToNeutral = rightAllianceToNeutral.mirror();
+
+        rightNeutralToAlliance = new BLinePathSequence(pathBuilder, FieldSymmetry.kRotational, "");
+        leftNeutralToAlliance = rightNeutralToAlliance.mirror();
+    }
+
+    // private boolean onLeftSide(Supplier<Pose2d> pose) {
+    //     if (pose.get().getY() AllianceFlipUtil.get(">", "<") FieldConstants.fieldWidth / 2) {
+
+    //     }
+    // }
+
+    // public BLinePathSequence getCurrentTrenchPath() {
+    //     if 
+    // }
+}
